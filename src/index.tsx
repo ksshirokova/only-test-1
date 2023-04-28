f85abac6befc8206ea20205f8b28f212a7565f36
Kseniia Shirokova <ksirokova85@gmail.com>
2023-04-28 20:42:55 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWorld=()=>
+    <div>Hey guys!!!!</div>
+
+
+export default HelloWorld;
---
+++ webpack.development.js
@@ -0,0 +1,27 @@
+module.exports = {
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    mode: "development",
+    entry: "./src/dev.tsx",
+    devServer: {
+        static: "./development",
+    },
+    module: {
+        rules: [
+            {
+                test: /.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                }
+            }
+        ]
+    }
+}
