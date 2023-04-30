ba9ee7c781522991e2e75fd4a4dbd70bb994c1f2
Kseniia Shirokova <ksirokova85@gmail.com>
2023-04-30 18:16:01 +0300

    fead: initial working state

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,17 +2,23 @@ import React from "react";
 import '../scss/index.scss'
 import Slider from "./slider";
 import Circle from "./circle";
+import { useState } from "react";
+import { scienceData } from "../utils/constants";
 
 
-const App = () =>
+
+function App(){
+    const [data, setData] = useState(scienceData)
     
-    <>
+    return(
     <main>
         <p>2015</p>
-        <Slider />
+        
         <Circle />
+        <Slider data={data}/>
     </main>
+    )
 
-    </>
-
+   
+}
 export default App;
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Swiper from 'react-id-swiper';
 import { useRef } from "react";
 
-export default function Slider() {
+export default function Slider({ data }) {
 
 
     const ref: any = useRef(null);
@@ -13,50 +13,44 @@ export default function Slider() {
         }
     };
 
-    const goPrev = () => {
-        if (ref.current !== null && ref.current.swiper !== null) {
-            ref.current.swiper.slidePrev();
-        }
-    };
+    // const goPrev = () => {
+    //     if (ref.current !== null && ref.current.swiper !== null) {
+    //         ref.current.swiper.slidePrev();
+    //     }
+    // };
+data.map(el=>console.log(el.year))
     return (
+
         <section className="slider-container">
             <section className="slider">
-                <Swiper ref={ref}>
 
+                <Swiper ref={ref} >
+                {data.map(el=>
+                   
                     <div className="slider-content">
-                        <h3>2015</h3>
-                        <p>13 сентября — частное солнечное затмение, видимое в Южной Африке и части Антарктиды</p>
+                        <h3>{el.year}</h3>
+                        <p>{el.text}</p>
                     </div>
-
-                    <div className="slider-content">
-                        <h3>2016</h3>
-                        <p>Телескоп «Хаббл» обнаружил самую удалённую из всех обнаруженных галактик, получившую обозначение GN-z11</p>
-                    </div>
-
-                    <div className="slider-content">
-                        <h3>2017</h3>
-                        <p>Компания Tesla официально представила первый в мире электрический грузовик Tesla Semi</p>
-                    </div>
-                    <div className="slider-content">
-                        <h3>2017</h3>
-                        <p>Компания Tesla официально представила первый в мире электрический грузовик Tesla Semi</p>
-                    </div>
-
+                    
+                
+                )}
 
                 </Swiper>
 
-                
+
 
 
             </section>
             <button onClick={goNext} className="button">
 
-                    <svg width="8" height="15" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M1 1L6 6L1 11" stroke="#3877EE" stroke-width="2" />
-                    </svg>
+                <svg width="8" height="15" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 1L6 6L1 11" stroke="#3877EE" stroke-width="2" />
+                </svg>
 
-                </button>
+            </button>
         </section>
+
+
 
 
 
