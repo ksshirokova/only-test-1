69e112aa92909fa12601ce1651b4bd056ec05c1a
Kseniia Shirokova <ksirokova85@gmail.com>
2023-05-01 18:49:10 +0300

    feat: first working v

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,58 +1,60 @@
-import React from "react";
-import Swiper from 'react-id-swiper';
+import React, { useEffect } from "react";
+import Swiper from "react-id-swiper";
 import { useRef } from "react";
+import { TData } from "../utils/types";
 
-export default function Slider({ data }) {
-
-
+export default function Slider({ data }: { data: TData[] }) {
     const ref: any = useRef(null);
-
+    let count = 0;
+   
     const goNext = () => {
-        if (ref.current !== null && ref.current.swiper !== null) {
+        count += 1
+        
+        if (ref.current !== null && ref.current.swiper !== null && data.length - 2 > count ) {
             ref.current.swiper.slideNext();
+           
         }
+       
     };
-
-    const goPrev = () => {
-        if (ref.current !== null && ref.current.swiper !== null) {
-            ref.current.swiper.slidePrev();
-        }
+    const linearAppearance = () => {
+        gsap.fromTo(
+            ref.current,
+            {
+                opacity: 0,
+            },
+            {
+                opacity: 1,
+                duration: 2,
+            }
+        );
     };
+    useEffect(() => {
+        linearAppearance();
+    });
 
     return (
-
         <section className="slider-container">
             <section className="slider">
-
-                <Swiper ref={ref} >
-                {data.map(el=>
-                   
-                    <div className="slider-content">
-                        <h3>{el.year}</h3>
-                        <p>{el.text}</p>
-                    </div>
-                    
-                
-                )}
-
+                <Swiper ref={ref}>
+                    {data.map((el: TData) => (
+                        <div className="slider-content">
+                            <h3>{el.year}</h3>
+                            <p>{el.text}</p>
+                        </div>
+                    ))}
                 </Swiper>
-
-
-
-
             </section>
             <button onClick={goNext} className="button">
-
-                <svg width="8" height="15" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 1L6 6L1 11" stroke="#3877EE" stroke-width="2" />
+                <svg
+                    width="8"
+                    height="15"
+                    viewBox="0 0 8 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M1 1L6 6L1 11" stroke="#3877EE" strokeWidth="2" />
                 </svg>
-
             </button>
         </section>
-
-
-
-
-
-    )
+    );
 }
